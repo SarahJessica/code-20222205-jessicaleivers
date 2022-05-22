9aa1013b9da5ede529918893a647ed8aba4be75c
Sarah Jessica Leivers <sarahjessicaleivers@Sarahs-MacBook-Pro-3.local>
2022-05-22 22:46:32 +0100

    feat: calculating risks

--- src/main.py
+++ src/main.py
@@ -1,2 +1,32 @@
-def hello_world():
+def calculate_bmi(weight, height):
+    # BMI(kg/m2) = mass(kg) / height(m)2
     return True
+
+def find_health_risk(bmi):
+    category = ''
+    risk = ''
+    if bmi <= 18.4:
+        category = 'Underweight'
+        risk = 'Malnutrition Risk'
+    elif bmi <25:
+        category = 'Normal weight'
+        risk = 'Low risk'
+    elif bmi <30:
+        category = 'Overweight'
+        risk = 'Enhanced Risk'
+    elif bmi < 35:
+        category = 'Moderately Obese'
+        risk = 'Medium Risk'
+    elif bmi < 40:
+        category = 'Severely Obese'
+        risk = 'High Risk'
+    else:
+        category = 'Very Severely Obese'
+        risk = 'Very High Risk'
+
+    return {
+        'category': category,
+        'health risk': risk
+    }
+
+
--- test/test_main.py
+++ test/test_main.py
@@ -1,4 +1,18 @@
+import pytest
+
 from src import main
 
-def test_hello_world():
-    assert main.hello_world() == True
+def test_calculate_bmi():
+    assert main.calculate_bmi(75,165) == True
+
+@pytest.mark.parametrize('bmi, category, risk', [
+    (5, 'Underweight','Malnutrition Risk'),
+    (20, 'Normal weight', 'Low risk')
+])
+def test_find_health_risk(bmi, category, risk):
+    actual = main.find_health_risk(bmi)
+    expected_resp = {
+        'category': category,
+        'health risk': risk
+    }
+    assert expected_resp == actual
